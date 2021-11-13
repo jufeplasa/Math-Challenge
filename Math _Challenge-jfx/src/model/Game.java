b2cafee0a8a0b04503f72f277b9dd13c2f26533b
Juan Felipe <jufeplasa@hotmail.com>
2021-11-13 17:55:28 -0500

    I add the method to generate the challenges

--- Math _Challenge-jfx/src/model/Game.java
+++ Math _Challenge-jfx/src/model/Game.java
@@ -6,7 +6,8 @@ public class Game {
 	private int failAnswer1;
 	private int failAnswer2;
 	private int failAnswer3;
-
+	private int a;
+	private int b;
 
 	public Game() {
 
@@ -28,26 +29,169 @@ public class Game {
 		return failAnswer3;
 	}
 
-	public void generateChallenge() {
-		int a =(int)Math.floor(Math.random()*99);
-		int b =(int)Math.floor(Math.random()*99);
-
-		int operation=(int)Math.floor(Math.random()*4);
-
-		if(operation==0) {
+	public int generateChallenge() {
+		a =(int)Math.floor(Math.random()*99);
+		b =(int)Math.floor(Math.random()*99);
+		int min;
+		int max;
+		int operation=(int)Math.floor(Math.random()*5);
+		switch(operation) {
+		case(0):
 			correctAnswer=a+b;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
 		}
-		else if(operation==1) {
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;
+		
+		case(1):
+			correctAnswer=a-b;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
+		}
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;
+		
+		case(2):
+		correctAnswer=a*b;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
+		}
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;
+		case(3):
+		correctAnswer=a/b;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
+		}
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;
+		case(4):
+			correctAnswer=a%b;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
+		}
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;
+		case(5):
+			correctAnswer=a^2;
+		min=correctAnswer-10;
+		max=correctAnswer+10;
+		failAnswer1=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer2=(int) Math.floor(Math.random()*(max-min)+min);
+		failAnswer3=(int) Math.floor(Math.random()*(max-min)+min);
+		if(failAnswer1==correctAnswer) {
+			failAnswer1++;
+		}
+		if(failAnswer2==correctAnswer) {
+			failAnswer2++;
+		}
+		if(failAnswer3==correctAnswer) {
+			failAnswer3--;
+		}
+		break;	
+
 
 		}
-		else if(operation==2) {
-
-		}
-		else if(operation==3) {
-
-		}
-		else if(operation==4) {
-
-		}
+		return operation;
 	}
-}
+
+		public String showOptions() {
+			String message="";
+			int option=(int)Math.floor(Math.random()*3);
+			switch(option) {
+			case(0):
+				message="A) "+correctAnswer+"           B) "+failAnswer1+"\n";
+				message+="C) "+failAnswer2+"           D) "+failAnswer3;
+				break;
+			case(1):
+				message="A) "+failAnswer2+"           B) "+correctAnswer+"\n";
+			message+="C) "+failAnswer3+"           C) "+failAnswer1;
+			break;
+			case(2):
+				message="A) "+failAnswer3+"           B) "+failAnswer2+"\n";
+			message+="C) "+failAnswer1+"           C) "+correctAnswer;
+			break;
+			case(3):
+				message="A) "+failAnswer1+"           B) "+failAnswer3+"\n";
+			message+="C) "+correctAnswer+"           D) "+failAnswer2;
+			break;
+			}
+			return message;
+		}
+
+		public String showCallenge() {
+			String message="";
+			int option=generateChallenge();
+			if(option==0) {
+				message=a+"+"+b;
+			}
+			else if(option==1) {
+				message=a+"-"+b;
+			}
+			else if(option==2) {
+				message=a+"*"+b;
+			}
+			else if(option==3) {
+				message=a+"/"+b;
+			}
+			else if(option==4) {
+				message=a+"%"+b;
+			}
+			else {
+				message=a+"^"+2;
+			}
+			return message;
+		}
+
+	}
--- Math _Challenge-jfx/src/ui/Main.java
+++ Math _Challenge-jfx/src/ui/Main.java
@@ -1,7 +1,24 @@
 package ui;
-
+import model.Game;
+import java.util.Scanner;
 public class Main {
+	private static Game game;
+	private static Scanner sc;
+	
 	public static void main(String[] args) {
-		
+		game=new Game();
+		sc=new Scanner(System.in);
+		boolean exit=false;
+		while(!exit) {
+			System.out.println("Presione 1");
+			int option=sc.nextInt();
+			sc.nextLine();
+			switch(option) {
+			case(1):
+				System.out.println("          "+game.showCallenge());
+				System.out.println(game.showOptions());
+				break;
+			}
+		}
 	}
 }
